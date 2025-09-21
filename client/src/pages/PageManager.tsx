c02a9cd185365278b03e3ff1fd15391aaaed013f
themilkpudding <114882101+themilkpudding@users.noreply.github.com>
2025-09-22 00:30:07 +0400

    Реализация карты переходов

--- client/src/pages/PageManager.tsx
+++ client/src/pages/PageManager.tsx
@@ -3,11 +3,24 @@ import React, { useState } from 'react';
 import Login from './Login/Login';
 import GamePage from './Game/Game';
 import NotFound from './NotFound/NotFound';
+import Menu from './Menu/Menu';
+import Registration from './Registration/Registration';
+import PasswordRecovery from './PasswordRecovery/PasswordRecovery';
+import ClassShop from './ClassShop/ClassShop';
+import StartingGameMenu from './StartingGameMenu/StartingGameMenu';
+import Lobby from './Lobby/Lobby';
+
 
 export enum PAGES {
     LOGIN,
     GAME,
     NOT_FOUND,
+    MENU,
+    PASSWORD_RECOVERY,
+    REGISTRATION,
+    CLASS_SHOP,
+    STARTING_GAME_MENU,
+    LOBBY
 }
 
 export interface IBasePage {
@@ -22,6 +35,12 @@ const PageManager: React.FC = () => {
             {page === PAGES.LOGIN && <Login setPage={setPage} />}
             {page === PAGES.GAME && <GamePage setPage={setPage} />}
             {page === PAGES.NOT_FOUND && <NotFound setPage={setPage} />}
+            {page === PAGES.MENU && <Menu setPage={setPage} />}
+            {page === PAGES.PASSWORD_RECOVERY && <PasswordRecovery setPage={setPage} />}
+            {page === PAGES.REGISTRATION && <Registration setPage={setPage} />}
+            {page === PAGES.CLASS_SHOP && <ClassShop setPage={setPage} />}
+            {page === PAGES.STARTING_GAME_MENU && <StartingGameMenu setPage={setPage} />}
+            {page === PAGES.LOBBY && <Lobby setPage={setPage} />}
         </>
     );
 }
